5d7820325f53acb411b1d1f98cca4ed9fd871774
trigonometr <62100465+trigonometr@users.noreply.github.com>
2021-01-06 21:34:18 +0300

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -19,26 +19,25 @@ int main() {
   std::vector<std::string> test_words;
   std::vector<bool> result;
 
+  // a^n * b^n
   productions = {{'S', "aSb"}, {'S', ""}};
   test_words = {"aaaaaaaa", "aabb", "bbbbbb", "aaabb", "abab", "", "aaaaaabbbbbb"};
   result = {false, true, false, false, false, true, true};
   Test(productions, test_words, result);
 
+  // Balanced seq
   productions = {{'S', "(S)S"}, {'S', ""}};
   test_words = {"(((((())))", "(()(()()))", ")", "", "(((()())())((()())()))", "()()))(()())((())()))((()())())()()())("};
   result = {false, true, false, true, true, false};
   Test(productions, test_words, result);
 
-  productions = {{'S', "(S)S"}, {'S', ""}};
-  test_words = {"(((((())))", "(()(()()))", ")", "", "(((()())())((()())()))", "()()))(()())((())()))((()())())()()())("};
-  result = {false, true, false, true, true, false};
-  Test(productions, test_words, result);
-
+  // |w|_a = |w|_b
   productions = {{'S', "aSbS"}, {'S', "bSaS"}, {'S', ""}};
   test_words = {"abaabaaaabbb", "abbbaababbaabaabaabaabbbbaaaabaaaabbbbbb", "ab", "ba", "bbbbbbbbbbbbbbaaaaaaaaaaaaaaa", ""};
   result = {false, true, true, true, false, true};
   Test(productions, test_words, result);
 
+  // arithmetic expressions x, y
   productions = {{'S', "P+S"}, {'S', "P"}, {'P', "F*P"},
                  {'P', "FP"}, {'P', "F"}, {'F', "x"},
                  {'F', "y"}, {'F', "(S)"}};
@@ -46,5 +45,19 @@ int main() {
   result = {true, true, true, false, true, true};
   Test(productions, test_words, result);
 
+  // w = w^R
+  productions = {{'S', "aSa"}, {'S', "bSb"}, {'S', "a"},
+                 {'S', "b"}, {'S', ""}};
+  test_words = {"ababababa", "", "bbaab", "bbaabb", "aaaaaaaaaaaaaaaaa", "bbbbababbbabbbba"};
+  result = {true, true, false, true, true, false};
+  Test(productions, test_words, result);
+
+  // a^n * b^m (n >= m)
+  productions = {{'S', "aS"}, {'S', "T"},
+                 {'T', "aTb"}, {'T', ""}};
+  test_words = {"aaaaabbb", "", "aaaaaabbbbbb", "aabbb", "aaaaabbbbbbb", "bbbbababbbabbbba"};
+  result = {true, true, true, false, false, false};
+  Test(productions, test_words, result);
+  
   return 0;
 }
